599e2af8e79746dfdc6e709d5e7b68c4c6c8b49f
Nikos Kostoulas <kostoulas@workable.com>
2017-10-11 19:09:34 +0300

    Refactor exec

--- src/lib/exec.ts
+++ src/lib/exec.ts
@@ -1,11 +1,11 @@
 import * as chalk from 'chalk';
-import { execSync as execCb } from 'child_process';
+import { execSync } from 'child_process';
 
 export default function(str, log = true, interactive = false) {
   log && console.log(chalk.yellow(`-> ${str}`));
   const options = { stdio: interactive ? 'inherit' : 'pipe' };
   try {
-    const output = execCb(str, options);
+    const output = execSync(str, options);
     if (output) {
       return output.toString().trim();
     }
